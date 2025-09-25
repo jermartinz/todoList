e6c27804845b1ae13beee8fb0f1b852a72dfc56b
Jeremy A. <61643086+jermartinz@users.noreply.github.com>
2025-09-25 04:06:34 -0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	todos := Todos{}
+	todos.add("Buy Milk")
+	todos.add("Buy Bread")
+	fmt.Printf("%+v\n\n", todos)
+	todos.delete(0)
+	fmt.Printf("%+v", todos)
+}
