47f2c3f88156103cc1f6747e981f632e5eaf5482
Jeremy A. <61643086+jermartinz@users.noreply.github.com>
2025-09-27 02:01:36 -0300

    feat:Add functionality to save todos to a json file

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 func main() {
 	todos := Todos{}
+	storage := NewStorage[Todos]("todos.json:")
+	storage.Load(&todos)
 	todos.add("Buy Milk")
 	todos.add("Buy Bread")
 	todos.toggle(0)
 	todos.print()
+	storage.Save(todos)
 }
